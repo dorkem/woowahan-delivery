7c5504cc7bcf4f7a42d9734a78beceb0ef926759
dorkem <20233144@gs.cwnu.ac.kr>
2026-01-22 22:05:52 +0900

    :sparkles: 주문 도메인 팩토리 메서드 형식으로 작성

--- src/main/java/com/dorkem/food/order/domain/Order.java
+++ src/main/java/com/dorkem/food/order/domain/Order.java
@@ -18,8 +18,8 @@ public class Order {
 	@Column(nullable = false)
 	private Long storeId;
 
-	@Column(nullable = false)
-	private Long addressId; // 주소가 삭제되었을 때를 고려해야함 (차라리 스냅샷?)
+	@Embedded
+	private DeliveryAddressSnapshot deliveryAddress;
 
 	@Enumerated(EnumType.STRING)
 	@Column(nullable = false, length = 30)
@@ -43,6 +43,74 @@ public class Order {
 	@Column(nullable = false)
 	private LocalDateTime createdAt;
 
-	protected Order() {}
+	protected Order() {
+	}
 
+	private Order(
+		Long accountId,
+		Long storeId,
+		DeliveryAddressSnapshot deliveryAddress,
+		Integer totalMenuAmount,
+		Integer deliveryFee,
+		String requestToStore,
+		String requestToRider
+	) {
+		this.accountId = accountId;
+		this.storeId = storeId;
+		this.deliveryAddress = deliveryAddress;
+
+		this.totalMenuAmount = totalMenuAmount;
+		this.deliveryFee = deliveryFee;
+		this.totalPayAmount = safeAdd(totalMenuAmount, deliveryFee);
+
+		this.requestToStore = requestToStore;
+		this.requestToRider = requestToRider;
+
+		this.orderStatus = OrderStatus.CREATED;
+		this.createdAt = LocalDateTime.now();
+	}
+
+	public static Order create(
+		Long accountId,
+		Long storeId,
+		DeliveryAddressSnapshot deliveryAddress,
+		Integer totalMenuAmount,
+		Integer deliveryFee,
+		String requestToStore,
+		String requestToRider
+	) {
+		if (accountId == null)
+			throw new IllegalArgumentException("accountId는 필수입니다.");
+		if (storeId == null)
+			throw new IllegalArgumentException("storeId는 필수입니다.");
+		if (deliveryAddress == null)
+			throw new IllegalArgumentException("배송지는 필수입니다.");
+		if (totalMenuAmount < 0 || deliveryFee < 0)
+			throw new IllegalArgumentException("금액은 0 이상이어야 합니다.");
+
+		return new Order(
+			accountId,
+			storeId,
+			deliveryAddress,
+			totalMenuAmount,
+			deliveryFee,
+			requestToStore,
+			requestToRider
+		);
+	}
+
+	private int safeAdd(int a, int b) {
+		long sum = (long)a + (long)b;
+		if (sum > Integer.MAX_VALUE) {
+			throw new IllegalArgumentException("금액 합계 초과");
+		}
+		return (int)sum;
+	}
+
+	public void changeStatus(OrderStatus next) {
+		if (!orderStatus.canTrans(next)) {
+			throw new IllegalStateException("상태 변경 불가: " + orderStatus + " -> " + next);
+		}
+		this.orderStatus = next;
+	}
 }
