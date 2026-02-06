c7e3cef0e3fdabd81faa439338a72b6d91700c16
dorkem <20233144@gs.cwnu.ac.kr>
2026-02-07 01:03:22 +0900

    🗑️필요없는 로직 제거

--- src/main/java/com/dorkem/food/order/query/repository/OrderItemReadRepository.java
+++
@@ -1,9 +0,0 @@
-package com.dorkem.food.order.query.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.dorkem.food.order.domain.read.OrderItemRead;
-import com.dorkem.food.order.domain.read.OrderItemReadId;
-
-public interface OrderItemReadRepository extends JpaRepository<OrderItemRead, OrderItemReadId> {
-}
