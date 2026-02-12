70063b7f53ad8b8ba2cbb5bd65a68dfc509e8698
dorkem <20233144@gs.cwnu.ac.kr>
2026-02-12 22:32:24 +0900

    :sparkles: Order-Menu n:m 임시로 생성

---
+++ src/main/java/com/dorkem/food/order/entity/OrderItem.java
@@ -0,0 +1,32 @@
+package com.dorkem.food.order.entity;
+
+import com.dorkem.food.menu.entity.Menu;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class OrderItem {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long orderItemId;
+
+	@ManyToOne
+	private Order order;
+
+	@ManyToOne
+	private Menu menu;
+	private int orderPrice;
+	private int quantity;
+}
