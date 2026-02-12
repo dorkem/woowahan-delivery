1fe4c642413e2a99dd086bd0232afc6a3e86707f
dorkem <20233144@gs.cwnu.ac.kr>
2026-02-12 22:31:42 +0900

    :sparkles: Menu 엔티티 생성

---
+++ src/main/java/com/dorkem/food/menu/entity/Menu.java
@@ -0,0 +1,38 @@
+package com.dorkem.food.menu.entity;
+
+import java.time.LocalDateTime;
+
+import com.dorkem.food.order.entity.OrderItem;
+import com.dorkem.food.store.entity.Store;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Menu {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long menuId;
+
+	@ManyToOne
+	private Store store;
+
+	private String menuName;
+	private String menuDescription;
+	private int price;
+	private LocalDateTime createdAt;
+	private LocalDateTime updatedAt;
+	private LocalDateTime deletedAt;
+}
