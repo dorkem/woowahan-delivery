6d1a6423e7edd97bb4803edce3e5819ed5e86f4e
dorkem <20233144@gs.cwnu.ac.kr>
2026-02-18 16:45:48 +0900

    :recycle: 레포지토리 타입 String 타입으로 변경

--- src/main/java/com/dorkem/food/order/repository/OrderRepository.java
+++ src/main/java/com/dorkem/food/order/repository/OrderRepository.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository;
 import com.dorkem.food.order.entity.Order;
 
 @Repository
-public interface OrderRepository extends JpaRepository<Order, Long> {
+public interface OrderRepository extends JpaRepository<Order, String> {
 }
