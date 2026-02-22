17deee410bc65f670a5fdc27022240f4ec7924f7
dorkem <20233144@gs.cwnu.ac.kr>
2026-02-22 23:25:35 +0900

    :recycle: 피드백 반영: View를 백엔드에서 따로 전달할 필요가 없을 것 같습니다.

--- src/main/java/com/dorkem/food/store/entity/StoreStatus.java
+++ src/main/java/com/dorkem/food/store/entity/StoreStatus.java
@@ -6,10 +6,8 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum StoreStatus {
-	OPEN("영업 중"),
-	CLOSED("영업 종료"),
-	BREAK_TIME("준비 중"),
-	TEMPORARY_PAUSE("일시 정지");
-
-	private final String description;
+	OPEN,
+	CLOSED,
+	BREAK_TIME,
+	TEMPORARY_PAUSE;
 }
