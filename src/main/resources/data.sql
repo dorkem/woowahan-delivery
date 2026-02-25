1f039cbee67b8a2f9bba9d2eedc678d11ccb6e80
dorkem <20233144@gs.cwnu.ac.kr>
2026-02-25 23:53:04 +0900

    :memo: data.sql 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
 INSERT INTO users (user_id, username, phone_number, email, password, login_type, user_type, created_at)
 VALUES (1, '최재혁', '010-1234-5678', 'user@test.com', 'password123', 'KAKAO', 'CUSTOMER', NOW());
 
-INSERT INTO stores (store_id, owner_user_id, store_name, business_number, store_address, store_address_details, status, min_order_amount, base_delivery_fee, created_at)
+INSERT INTO stores (store_id, owner_id, store_name, business_number, store_address, store_address_details, status, min_order_amount, base_delivery_fee, created_at)
 VALUES (1, 1, 'BBQ', '123-45-67890', '서울특별시 강남구', '15층', 'OPEN', 15000, 3000, NOW());
 
 INSERT INTO menus (menu_id, store_store_id, menu_name, menu_description, price, created_at)
