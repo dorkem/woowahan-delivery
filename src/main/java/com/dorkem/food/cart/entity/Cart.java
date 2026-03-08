e9d29fb72479ecd5776edb1ef35993f05398ad01
dorkem <20233144@gs.cwnu.ac.kr>
2026-03-08 20:31:58 +0900

    :sparkles: 카트, 장바구니 엔티티 구현

---
+++ src/main/java/com/dorkem/food/cart/entity/Cart.java
@@ -0,0 +1,74 @@
+package com.dorkem.food.cart.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.dorkem.food.store.entity.Store;
+import com.dorkem.food.user.entity.Customer;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "carts")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Cart {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "cart_id")
+	private Long cartId;
+
+	@OneToOne
+	@JoinColumn(name = "customer_id", nullable = false, unique = true)
+	private Customer customer;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "store_id")
+	private Store store;
+
+	@OneToMany(mappedBy = "cart", cascade = CascadeType.ALL)
+	private List<CartItem> items = new ArrayList<>();
+
+	private Cart(Customer customer) {
+		this.customer = customer;
+	}
+
+	public static Cart createCart(Customer customer) {
+		return new Cart(customer);
+	}
+
+	public void addItem(Store store, CartItem item) {
+		if (this.store != null && !this.store.getStoreId().equals(store.getStoreId())) {
+			this.items.clear();
+		}
+		this.store = store;
+		this.items.add(item);
+		item.assignCart(this);
+	}
+
+	public void clearItems() {
+		this.items.clear();
+		this.store = null;
+	}
+
+	public int getTotalPrice() {
+		return items.stream()
+			.mapToInt(CartItem::getSubtotal)
+			.sum();
+	}
+}
---
+++ src/main/java/com/dorkem/food/cart/entity/CartItem.java
@@ -0,0 +1,61 @@
+package com.dorkem.food.cart.entity;
+
+import com.dorkem.food.menu.entity.Menu;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "cart_items")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class CartItem {
+
+	@Id
+	@Getter
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "cart_item_id")
+	private Long cartItemId;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "cart_id", nullable = false)
+	private Cart cart;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "menu_id", nullable = false)
+	private Menu menu;
+
+	@Column(name = "quantity", nullable = false)
+	private int quantity;
+
+	private CartItem(Menu menu, int quantity){
+		this.menu = menu;
+		this.quantity = quantity;
+	}
+
+	public static CartItem createCartItem(Menu menu, int quantity){
+		return new CartItem(menu, quantity);
+	}
+
+	public void assignCart(Cart cart){
+		this.cart = cart;
+	}
+
+	public void updateQuantity(int quantity){
+		this.quantity += quantity;
+	}
+
+	public int getSubtotal() {
+		return this.menu.getPrice() * this.quantity;
+	}
+}
