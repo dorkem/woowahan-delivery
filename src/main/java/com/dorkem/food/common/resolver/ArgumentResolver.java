b669acdf0c02ae6313b7a549d85a0544620c01a4
dorkem <20233144@gs.cwnu.ac.kr>
2026-03-14 11:57:40 +0900

    :sparkles: filter로 jwt 인증 구현

---
+++ src/main/java/com/dorkem/food/common/resolver/ArgumentResolver.java
@@ -0,0 +1,30 @@
+package com.dorkem.food.common.resolver;
+
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import com.dorkem.food.common.annotation.AuthUserId;
+
+@Component
+public class ArgumentResolver implements HandlerMethodArgumentResolver {
+
+	@Override
+	public boolean supportsParameter(MethodParameter parameter) {
+		return parameter.hasParameterAnnotation(AuthUserId.class)
+			&& parameter.getParameterType().equals(Long.class);
+	}
+
+	@Override
+	public Object resolveArgument(
+		MethodParameter parameter,
+		ModelAndViewContainer mavContainer,
+		NativeWebRequest webRequest,
+		WebDataBinderFactory binderFactory) throws Exception {
+
+		return null;
+	}
+}
