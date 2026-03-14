6c6424190c9093f8596d32f9ccaa486e85b3527b
dorkem <20233144@gs.cwnu.ac.kr>
2026-03-14 12:13:36 +0900

    :sparkles: (변경예정) 유저아이디로 커스터머 찾기

--- src/main/java/com/dorkem/food/user/repository/CustomerRepository.java
+++ src/main/java/com/dorkem/food/user/repository/CustomerRepository.java
@@ -1,5 +1,7 @@
 package com.dorkem.food.user.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +9,7 @@ import com.dorkem.food.user.entity.Customer;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+	// 변경예정입니당
+	Optional<Customer> findByUser_UserId(Long userId);
 }
