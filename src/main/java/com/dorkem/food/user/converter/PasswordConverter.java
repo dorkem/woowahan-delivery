b6f7c6110dbb52ffd8218e7a0afc07a9fdc9a087
dorkem <20233144@gs.cwnu.ac.kr>
2026-04-17 20:19:21 +0900

    :recycle: Converter사용으로 Password를 평문으로 저장하는게 아니라 BCrypt로 암호화하여 저장

---
+++ src/main/java/com/dorkem/food/user/converter/PasswordConverter.java
@@ -0,0 +1,20 @@
+package com.dorkem.food.user.converter;
+
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
+@Converter
+public class PasswordConverter implements AttributeConverter<String, String> {
+
+	@Override
+	public String convertToDatabaseColumn(String password) {
+		return BCrypt.withDefaults().hashToString(12, password.toCharArray());
+	}
+
+	@Override
+	public String convertToEntityAttribute(String dbPassword) {
+		return dbPassword;
+	}
+}
