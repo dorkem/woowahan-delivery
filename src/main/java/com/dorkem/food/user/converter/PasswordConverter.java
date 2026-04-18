1c26e853686dacd9d16483f776bc153b69ef46fd
dorkem <20233144@gs.cwnu.ac.kr>
2026-04-18 11:17:17 +0900

    :recycle: OAuth 로그인시 PW 제외해도 되도록

--- src/main/java/com/dorkem/food/user/converter/PasswordConverter.java
+++ src/main/java/com/dorkem/food/user/converter/PasswordConverter.java
@@ -10,6 +10,7 @@ public class PasswordConverter implements AttributeConverter<String, String> {
 
 	@Override
 	public String convertToDatabaseColumn(String password) {
+		if (password == null) return null;
 		return BCrypt.withDefaults().hashToString(12, password.toCharArray());
 	}
 
